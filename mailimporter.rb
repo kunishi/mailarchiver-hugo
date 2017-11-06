5ddc24fe7f479b448fd4b9382e4d9f3295a8340d
Takeo Kunishima <t.kunishi@gmail.com>
2017-11-06 17:16:43 +0900

    remove Jekyll-based files.

--- mailimporter.rb
+++ mailimporter.rb
@@ -9,7 +9,8 @@ yaml = YAML::load(File.open('_config.yml'))
 blogs = yaml['blog']
 blogs.each do |blog|
   blog['assets_dir'] ||= 'assets/dbjapan'
-  blog['posts_dir'] ||= '_posts'
+  #blog['posts_dir'] ||= '_posts'
+  blog['posts_dir'] ||= '../dbjapan-hugo/content/ml_archives'
 
   if ARGV[0]
     filename = ARGV[0]
@@ -25,9 +26,9 @@ blogs.each do |blog|
   date = mail.date
   post_id = nil
   if message_id
-    post_id = "#{date.strftime("%Y-%02m-%02d")}-#{Digest::MD5.hexdigest(message_id)}"
+    post_id = "#{Digest::MD5.hexdigest(message_id)}"
   else
-    post_id = "#{date.strftime("%Y-%02m-%02d")}-#{Digest::MD5.hexdigest(mail.body.to_s)}"
+    post_id = "#{Digest::MD5.hexdigest(mail.body.to_s)}"
   end
   post_ext = ".md"
   post_filename = blog['posts_dir'] + "/" + post_id + post_ext
@@ -35,10 +36,10 @@ blogs.each do |blog|
   subject = Kconv.toutf8(mail.subject.gsub(/"/, '\"'))
 
   if mail.multipart?
-    if mail.html_part
-      body = Kconv.toutf8(mail.html_part.decoded)
-    elsif mail.text_part
+    if mail.text_part
       body = Kconv.toutf8(mail.text_part.decoded)
+    elsif mail.html_part
+      body = Kconv.toutf8(mail.html_part.decoded)
     end
   else
     body = Kconv.toutf8(mail.body.decoded)
@@ -46,9 +47,9 @@ blogs.each do |blog|
 
   open(post_filename, "w") do |file|
     file << "---\n"
-    file << "layout: post\n"
     file << "title: \"#{subject}\"\n"
-    file << "date: #{date.strftime("%Y-%m-%d %H:%M:%S %z")}\n"
+    file << "date: #{date.to_s}\n"
+    file << "type: ml_archive\n"
     file << "---\n"
     file << "<pre>\n"
     file << body
